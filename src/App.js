2c6e87e56bdc1744184031a092e985e2ada827fd
Yuval Kaplan <kapyuval@gmail.com>
2025-08-17 19:24:58 +0300

    Added some more configmap support

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [snippet, setSnippet] = useState('');
+
   useEffect(() => {
     fetch('/config/snippet.html')
       .then((res) => res.text())
-      .then((html) => {
-        const container = document.getElementById('injected-snippet');
-        if (container) {
-          container.innerHTML = html;
-        }
+      .then((html) => setSnippet(html))
+      .catch((err) => {
+        console.error('Failed to load snippet:', err);
+        setSnippet('<p>Failed to load snippet.</p>');
       });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div id="injected-snippet" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div dangerouslySetInnerHTML={{ __html: snippet }} />
   );
 }
 
